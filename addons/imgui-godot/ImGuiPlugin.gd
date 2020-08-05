586642dd383cd4a0a103016583a99b97c4d09fe9
Patrick Dawson <pat@dw.is>
2020-08-05 19:31:34 +0200

    Attempt to make plugin installation much easier

---
+++ addons/imgui-godot/ImGuiPlugin.gd
@@ -0,0 +1,62 @@
+tool
+extends EditorPlugin
+
+func _enter_tree():
+    add_custom_type("ImGuiNode", "Node2D", preload("ImGuiNode.cs"), preload("icon.tres"))
+
+    var name = ProjectSettings.get_setting("application/config/name")
+    fix_csproj("res://" + name + ".csproj")
+
+func _exit_tree():
+    remove_custom_type("ImGuiNode")
+
+func fix_csproj(fn):
+    # this is very silly, never do this
+    var needSources = true
+    var needNuget = true
+    var needUnsafe = true
+
+    var fi = File.new()
+    if fi.open(fn, File.READ) != OK:
+        printerr("imgui-godot: could not open " + fn)
+        return
+
+    var fileLines = []
+    while not fi.eof_reached():
+        var line = fi.get_line()
+        fileLines.append(line)
+
+        if line.find("<Compile") != -1:
+            if line.find("ImGuiNode.cs") != -1:
+                needSources = false
+        elif line.find("<PackageReference") != -1:
+            if line.find("ImGui.NET") != -1:
+                needNuget = false
+        elif line.find("<AllowUnsafeBlocks>true</AllowUnsafeBlocks>") != -1:
+            needUnsafe = false
+    fi.close()
+    
+    if needSources or needNuget or needUnsafe:
+        print("imgui-godot: fixing ", fn)
+    else:
+        return
+
+    fi.open(fn, File.WRITE)
+    for line in fileLines:
+        if needSources and line.find("<Compile") != -1:
+            needSources = false
+            fi.store_line("    <Compile Include=\"addons\\imgui-godot\\ImGuiGD.cs\" />")
+            fi.store_line("    <Compile Include=\"addons\\imgui-godot\\ImGuiNode.cs\" />")
+            fi.store_line(line)
+        elif needNuget and line.find("<PackageReference") != -1:
+            needNuget = false
+            fi.store_line("    <PackageReference Include=\"ImGui.NET\">")
+            fi.store_line("      <Version>1.75.0</Version>")
+            fi.store_line("    </PackageReference>")
+            fi.store_line(line)
+        elif needUnsafe and line.find("<ConsolePause>") != -1:
+            fi.store_line(line)
+            fi.store_line("    <AllowUnsafeBlocks>true</AllowUnsafeBlocks>")
+        else:
+            fi.store_line(line)
+    fi.close()
