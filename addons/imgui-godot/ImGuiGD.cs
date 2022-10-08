2401efeab76834cf44eca2a7dc1286fe884f9a6c
Patrick Dawson <pat@dw.is>
2022-10-08 19:32:45 +0200

    move into our own namespace

--- addons/imgui-godot/ImGuiGD.cs
+++ addons/imgui-godot/ImGuiGD.cs
@@ -2,75 +2,78 @@ using Godot;
 using ImGuiNET;
 using System;
 
-public static class ImGuiGD
+namespace ImGuiGodot
 {
-    public static float JoyAxisDeadZone { get; set; } = 0.15f;
-    public static bool JoyButtonSwapAB { get; set; } = false;
-
-    public static IntPtr BindTexture(Texture2D tex)
+    public static class ImGuiGD
     {
-        return ImGuiGDInternal.BindTexture(tex);
-    }
+        public static float JoyAxisDeadZone { get; set; } = 0.15f;
+        public static bool JoyButtonSwapAB { get; set; } = false;
 
-    public static void UnbindTexture(IntPtr texid)
-    {
-        ImGuiGDInternal.UnbindTexture(texid);
-    }
-
-    public static void Init()
-    {
-        if (IntPtr.Size != sizeof(ulong))
+        public static IntPtr BindTexture(Texture2D tex)
         {
-            GD.PrintErr("imgui-godot requires 64-bit pointers");
+            return ImGuiGDInternal.BindTexture(tex);
         }
-        ImGuiGDInternal.Init();
-    }
 
-    public static ImFontPtr AddFont(FontFile fontData, float fontSize, bool merge = false)
-    {
-        return ImGuiGDInternal.AddFont(fontData, fontSize, merge);
-    }
+        public static void UnbindTexture(IntPtr texid)
+        {
+            ImGuiGDInternal.UnbindTexture(texid);
+        }
 
-    public static ImFontPtr AddFont(FontFile fontData, float fontSize, IntPtr glyphRanges, bool merge = false)
-    {
-        return ImGuiGDInternal.AddFont(fontData, fontSize, glyphRanges, merge);
-    }
+        public static void Init()
+        {
+            if (IntPtr.Size != sizeof(ulong))
+            {
+                GD.PrintErr("imgui-godot requires 64-bit pointers");
+            }
+            ImGuiGDInternal.Init();
+        }
 
-    public static ImFontPtr AddFontDefault()
-    {
-        // right now this is a simple wrapper, but that will change
-        return ImGui.GetIO().Fonts.AddFontDefault();
-    }
+        public static ImFontPtr AddFont(FontFile fontData, float fontSize, bool merge = false)
+        {
+            return ImGuiGDInternal.AddFont(fontData, fontSize, merge);
+        }
 
-    // only call this once, shortly after Init
-    public static void RebuildFontAtlas()
-    {
-        ImGuiGDInternal.RebuildFontAtlas();
-    }
+        public static ImFontPtr AddFont(FontFile fontData, float fontSize, IntPtr glyphRanges, bool merge = false)
+        {
+            return ImGuiGDInternal.AddFont(fontData, fontSize, glyphRanges, merge);
+        }
 
-    public static void Update(double delta, Viewport vp)
-    {
-        ImGuiGDInternal.Update(delta, vp);
-    }
+        public static ImFontPtr AddFontDefault()
+        {
+            // right now this is a simple wrapper, but that will change
+            return ImGui.GetIO().Fonts.AddFontDefault();
+        }
 
-    public static void Render(RID parent)
-    {
-        ImGui.Render();
-        ImGuiGDInternal.RenderDrawData(ImGui.GetDrawData(), parent);
-    }
+        // only call this once, shortly after Init
+        public static void RebuildFontAtlas()
+        {
+            ImGuiGDInternal.RebuildFontAtlas();
+        }
 
-    public static void Shutdown()
-    {
-        ImGuiGDInternal.ClearCanvasItems();
-        if (ImGui.GetCurrentContext() != IntPtr.Zero)
-            ImGui.DestroyContext();
-    }
+        public static void Update(double delta, Viewport vp)
+        {
+            ImGuiGDInternal.Update(delta, vp);
+        }
 
-    /// <returns>
-    /// True if the InputEvent was consumed
-    /// </returns>
-    public static bool ProcessInput(InputEvent evt)
-    {
-        return ImGuiGDInternal.ProcessInput(evt);
+        public static void Render(RID parent)
+        {
+            ImGui.Render();
+            ImGuiGDInternal.RenderDrawData(ImGui.GetDrawData(), parent);
+        }
+
+        public static void Shutdown()
+        {
+            ImGuiGDInternal.ClearCanvasItems();
+            if (ImGui.GetCurrentContext() != IntPtr.Zero)
+                ImGui.DestroyContext();
+        }
+
+        /// <returns>
+        /// True if the InputEvent was consumed
+        /// </returns>
+        public static bool ProcessInput(InputEvent evt)
+        {
+            return ImGuiGDInternal.ProcessInput(evt);
+        }
     }
 }
--- addons/imgui-godot/ImGuiLayer.cs
+++ addons/imgui-godot/ImGuiLayer.cs
@@ -1,135 +1,138 @@
 using Godot;
 
-public partial class ImGuiLayer : CanvasLayer
+namespace ImGuiGodot
 {
-    public static ImGuiLayer Instance { get; private set; }
-
-    [Export]
-    public FontFile Font = null;
-
-    [Export]
-    public int FontSize = 16;
-
-    [Export]
-    public FontFile ExtraFont1 = null;
-
-    [Export]
-    public int ExtraFont1Size = 16;
-
-    [Export]
-    public FontFile ExtraFont2 = null;
-
-    [Export]
-    public int ExtraFont2Size = 16;
-
-    [Export]
-    public bool MergeFonts = true;
-
-    [Export]
-    public bool AddDefaultFont = true;
-
-    [Signal]
-    public delegate void ImGuiLayoutEventHandler();
-
-    private RID _canvasItem;
-
-    private partial class UpdateFirst : Node
+    public partial class ImGuiLayer : CanvasLayer
     {
+        public static ImGuiLayer Instance { get; private set; }
+
+        [Export]
+        public FontFile Font = null;
+
+        [Export]
+        public int FontSize = 16;
+
+        [Export]
+        public FontFile ExtraFont1 = null;
+
+        [Export]
+        public int ExtraFont1Size = 16;
+
+        [Export]
+        public FontFile ExtraFont2 = null;
+
+        [Export]
+        public int ExtraFont2Size = 16;
+
+        [Export]
+        public bool MergeFonts = true;
+
+        [Export]
+        public bool AddDefaultFont = true;
+
+        [Signal]
+        public delegate void ImGuiLayoutEventHandler();
+
+        private RID _canvasItem;
+
+        private partial class UpdateFirst : Node
+        {
+            public override void _EnterTree()
+            {
+                ProcessPriority = int.MinValue;
+            }
+
+            public override void _Process(double delta)
+            {
+                ImGuiGD.Update(delta, GetViewport());
+            }
+        }
+
         public override void _EnterTree()
         {
-            ProcessPriority = int.MinValue;
+            Instance = this;
+
+            ProcessPriority = int.MaxValue;
+            _canvasItem = RenderingServer.CanvasItemCreate();
+            RenderingServer.CanvasItemSetParent(_canvasItem, GetCanvas());
+            VisibilityChanged += OnChangeVisibility;
+
+            ImGuiGD.Init();
+            if (Font is not null)
+            {
+                ImGuiGD.AddFont(Font, FontSize);
+                if (ExtraFont1 is not null)
+                {
+                    ImGuiGD.AddFont(ExtraFont1, ExtraFont1Size, MergeFonts);
+                }
+                if (ExtraFont2 is not null)
+                {
+                    ImGuiGD.AddFont(ExtraFont2, ExtraFont2Size, MergeFonts);
+                }
+            }
+
+            if (AddDefaultFont)
+            {
+                ImGuiGD.AddFontDefault();
+            }
+            ImGuiGD.RebuildFontAtlas();
+
+            AddChild(new UpdateFirst());
+        }
+
+        public override void _Ready()
+        {
+            OnChangeVisibility();
+        }
+
+        public override void _ExitTree()
+        {
+            ImGuiGD.Shutdown();
+            RenderingServer.FreeRid(_canvasItem);
+        }
+
+        private void OnChangeVisibility()
+        {
+            RenderingServer.CanvasItemSetVisible(_canvasItem, Visible);
+            if (Visible)
+            {
+                ProcessMode = ProcessModeEnum.Always;
+                SetProcessInput(true);
+            }
+            else
+            {
+                ProcessMode = ProcessModeEnum.Disabled;
+                SetProcessInput(false);
+                ImGuiGDInternal.ClearCanvasItems();
+            }
         }
 
         public override void _Process(double delta)
         {
-            ImGuiGD.Update(delta, GetViewport());
+            EmitSignal(SignalName.ImGuiLayout);
+            ImGuiGD.Render(_canvasItem);
         }
-    }
 
-    public override void _EnterTree()
-    {
-        Instance = this;
-
-        ProcessPriority = int.MaxValue;
-        _canvasItem = RenderingServer.CanvasItemCreate();
-        RenderingServer.CanvasItemSetParent(_canvasItem, GetCanvas());
-        VisibilityChanged += OnChangeVisibility;
-
-        ImGuiGD.Init();
-        if (Font is not null)
+        public override void _Notification(long what)
         {
-            ImGuiGD.AddFont(Font, FontSize);
-            if (ExtraFont1 is not null)
+            ImGuiGDInternal.ProcessNotification(what);
+        }
+
+        public override void _Input(InputEvent e)
+        {
+            if (ImGuiGD.ProcessInput(e))
             {
-                ImGuiGD.AddFont(ExtraFont1, ExtraFont1Size, MergeFonts);
-            }
-            if (ExtraFont2 is not null)
-            {
-                ImGuiGD.AddFont(ExtraFont2, ExtraFont2Size, MergeFonts);
+                GetViewport().SetInputAsHandled();
             }
         }
 
-        if (AddDefaultFont)
+        public void Connect(ImGuiLayoutEventHandler d)
         {
-            ImGuiGD.AddFontDefault();
+            // temporary workaround for a bug in the early Godot 4 betas
+            Connect(SignalName.ImGuiLayout, new Callable(d.Target as Object, d.Method.Name));
+
+            // TODO: use this after Godot 4.0-beta3 is released
+            // ImGuiLayout += d;
         }
-        ImGuiGD.RebuildFontAtlas();
-
-        AddChild(new UpdateFirst());
-    }
-
-    public override void _Ready()
-    {
-        OnChangeVisibility();
-    }
-
-    public override void _ExitTree()
-    {
-        ImGuiGD.Shutdown();
-        RenderingServer.FreeRid(_canvasItem);
-    }
-
-    private void OnChangeVisibility()
-    {
-        RenderingServer.CanvasItemSetVisible(_canvasItem, Visible);
-        if (Visible)
-        {
-            ProcessMode = ProcessModeEnum.Always;
-            SetProcessInput(true);
-        }
-        else
-        {
-            ProcessMode = ProcessModeEnum.Disabled;
-            SetProcessInput(false);
-            ImGuiGDInternal.ClearCanvasItems();
-        }
-    }
-
-    public override void _Process(double delta)
-    {
-        EmitSignal(SignalName.ImGuiLayout);
-        ImGuiGD.Render(_canvasItem);
-    }
-
-    public override void _Notification(long what)
-    {
-        ImGuiGDInternal.ProcessNotification(what);
-    }
-
-    public override void _Input(InputEvent e)
-    {
-        if (ImGuiGD.ProcessInput(e))
-        {
-            GetViewport().SetInputAsHandled();
-        }
-    }
-
-    public void Connect(ImGuiLayoutEventHandler d)
-    {
-        // temporary workaround for a bug in the early Godot 4 betas
-        Connect(SignalName.ImGuiLayout, new Callable(d.Target as Object, d.Method.Name));
-
-        // TODO: use this after Godot 4.0-beta3 is released
-        // ImGuiLayout += d;
     }
 }
