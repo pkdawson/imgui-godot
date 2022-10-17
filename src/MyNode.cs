c3cf558e057d16b06586df3ce55d78fc37e3b2b2
Patrick Dawson <pat@dw.is>
2022-10-17 17:26:56 +0200

    demo: content scale modes

--- src/MyNode.cs
+++ src/MyNode.cs
@@ -3,6 +3,13 @@ using ImGuiNET;
 
 public partial class MyNode : Node
 {
+    private Window _window;
+
+    public override void _Ready()
+    {
+        _window = (Window)GetViewport();
+    }
+
     public override void _Process(double delta)
     {
         ImGui.ShowDemoWindow();
@@ -11,5 +18,16 @@ public partial class MyNode : Node
     private void _on_button1_pressed()
     {
         GetTree().ChangeSceneToFile("res://data/demo2.tscn");
+    }
+
+    private void OnContentScaleCIE()
+    {
+        _window.ContentScaleMode = Window.ContentScaleModeEnum.CanvasItems;
+        _window.ContentScaleAspect = Window.ContentScaleAspectEnum.Expand;
+    }
+
+    private void OnContentScaleDisabled()
+    {
+        _window.ContentScaleMode = Window.ContentScaleModeEnum.Disabled;
     }
 }
