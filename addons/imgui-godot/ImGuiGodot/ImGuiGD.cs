f972296249dcf56a2f2b8a00af50811ba81cbb12
Patrick Dawson <pat@dw.is>
2023-10-29 04:46:27 +0100

    workflow: name package artifact

--- addons/imgui-godot/ImGuiGodot/ImGuiGD.cs
+++ addons/imgui-godot/ImGuiGodot/ImGuiGD.cs
@@ -58,7 +58,7 @@ public static class ImGuiGD
     private static float _scale = 1.0f;
 
     private static bool _inProcessFrame = false;
-    
+
     public static bool Visible
     {
         get => _visible;
