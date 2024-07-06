2824263ed70c6a6e06d51413a38745db50254728
Patrick Dawson <pat@dw.is>
2024-07-07 00:40:02 +0200

    GdsDemo: add topmost window

--- doc/examples/GdsDemo/demo.gd
+++ doc/examples/GdsDemo/demo.gd
@@ -6,6 +6,7 @@ var values := [2.0, 4.0, 0.0, 3.0, 1.0, 5.0]
 var items := ["zero", "one", "two", "three"]
 var current_item := [2]
 var anim_counter := 0
+var wc_topmost: ImGuiWindowClassPtr
 
 func _ready():
     var io := ImGui.GetIO()
@@ -15,7 +16,12 @@ func _ready():
     style.Colors[ImGui.Col_PlotHistogram] = Color.REBECCA_PURPLE
     style.Colors[ImGui.Col_PlotHistogramHovered] = Color.SLATE_BLUE
 
+    wc_topmost = ImGuiWindowClassPtr.new()
+    wc_topmost.ViewportFlagsOverrideSet = ImGui.ViewportFlags_TopMost
+
 func _process(_delta: float) -> void:
+    ImGui.ShowDemoWindow()
+
     ImGui.Begin("hello")
     ImGui.Text("hello from GDScript")
     ImGui.DragFloat("myfloat", myfloat)
@@ -28,6 +34,12 @@ func _process(_delta: float) -> void:
     ImGui.ListBox("choices", current_item, items, items.size())
     ImGui.Combo("combo", current_item, items)
     ImGui.Text("choice = %s" % items[current_item[0]])
+    ImGui.End()
+
+    ImGui.SetNextWindowClass(wc_topmost)
+    ImGui.SetNextWindowSize(Vector2(200, 200), ImGui.Cond_Once)
+    ImGui.Begin("topmost viewport window")
+    ImGui.TextWrapped("when this is a viewport window outside the main window, it will stay on top")
     ImGui.End()
 
 func _physics_process(_delta: float) -> void:
