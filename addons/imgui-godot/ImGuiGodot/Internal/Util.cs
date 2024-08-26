b137a7a1d6458edb475760416f0bfa495def2155
Patrick Dawson <pat@dw.is>
2024-08-26 22:00:59 +0200

    Add AOT-compatible implementation of ConstructRid

--- addons/imgui-godot/ImGuiGodot/Internal/Util.cs
+++ addons/imgui-godot/ImGuiGodot/Internal/Util.cs
@@ -1,13 +1,20 @@
-#if GODOT_PC
 using Godot;
 using System;
+#if GODOT_PC
 using System.Reflection;
 using System.Reflection.Emit;
+#else
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+#endif
 
 namespace ImGuiGodot.Internal;
 
 internal static class Util
 {
+#if GODOT_PC
+    // this is ~15x faster, so keep it for non-AOT platforms
+
     public static readonly Func<ulong, Rid> ConstructRid;
 
     static Util()
@@ -23,5 +30,16 @@ internal static class Util
         il.Emit(OpCodes.Ret);
         ConstructRid = dm.CreateDelegate<Func<ulong, Rid>>();
     }
-}
+#else
+    public static Rid ConstructRid(ulong id)
+    {
+        Debug.Assert(Marshal.SizeOf<Rid>() == sizeof(ulong));
+        nint ptr = Marshal.AllocHGlobal(Marshal.SizeOf<Rid>());
+        byte[] bytes = BitConverter.GetBytes(id);
+        Marshal.Copy(bytes, 0, ptr, bytes.Length);
+        Rid rv = Marshal.PtrToStructure<Rid>(ptr);
+        Marshal.FreeHGlobal(ptr);
+        return rv;
+    }
 #endif
+}
